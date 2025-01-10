069d74357efd966ff585fb63bf1dee9b66f7d965
Meena Modhvadia <meena.modhvadia@digital.justice.gov.uk>
2025-01-10 23:51:59 +0000

    second iteration

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,27 +1,27 @@
 class LogLineParser
+
+  attr_reader :line
+  attr_reader :message
+
   def initialize(line)
+    message = ''
     @line = line
   end
 
   def message
-    message = @line.split(":")
-    message[1].strip
+    message_parts = @line.split(':')
+    "%s" % [@line.split(':')[1].strip]
   end
 
   def log_level
-
-    log_level_end = @line.index(']')
-    #debug "The value is #{log_level_end}."
-
-    log_level_start = @line.index('[')
-    #debug "The value is #{log_level_start}."
-    log_level = @line.slice(log_level_start+1,log_level_end-1).downcase
-
+    log_level = "%s" % [@line.split(':')[0].strip]
+    log_level.tr('[]', '').downcase
   end
 
   def reformat
     #debug "The value is #{message}."
     #debug "The value is #{log_level}."
-    message + ' ' + '(' + log_level + ')'
+    message << " (#{log_level})"
   end
+
 end
